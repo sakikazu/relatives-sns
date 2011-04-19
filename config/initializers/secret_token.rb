e47c9cd202d452c1a55ecc14bbb2817cc97a7745
sakikazu <sakikazu@dti-vps-srv14.(none)>
2011-04-19 14:23:59 +0900

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ADanRails3::Application.config.secret_token = 'ecc518e075be9b2628e6ec20456e29ab6acf73bd8fa77c71bcd3436c8817d29b1c90abbaa8cba006ed3d4f57f55a61771626f21bbee2dc6750926f5f87ed43e9'
