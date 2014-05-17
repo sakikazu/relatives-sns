e072092298aac4afab3dcb66c13d7a6a2e842a7b
Kazutaka Sakimura <sakikazu15@gmail.com>
2014-05-17 21:37:54 +0900

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.middleware.use ExceptionNotifier,
+  config.middleware.use ExceptionNotification::Rack,
+    :email => {
     :email_prefix => "[AdanHP] ", # Subjectのprefix
     :sender_address => %("AdanHP_notifier" <notifier@example.com>), # 送信元 ※これは適当アドレスだが、environment.rbでGmailの設定をしているのでGmailが送信元になって送られた
     :exception_recipients => %w(sakikazu15@gmail.com) # あて先の配列
+  }
 end
