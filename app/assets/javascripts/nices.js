5b4175e343cc83a2400dcae045c7ab545c9693e7
k.sakimura <sakikazu15@gmail.com>
2019-03-04 18:27:00 +0900

    turbolinksの挙動を理解できたので修正 - jsはbodyでなくhead内で行うこと（ソースコメントに記載） - DOMをappendする処理は、事前にクリアしてから行う - ページ内遷移では、turbolinksがサーバーリクエストを行わないようにするため、turbolinksを切る

--- app/assets/javascripts/nices.js
+++ app/assets/javascripts/nices.js
@@ -9,34 +9,43 @@ $(document).on('turbolinks:load', function() {
   //目次を作成する
   //todo もっと汎用性あるようにしたい。「nice～」とかあるし
   $.fn.mokuji = function() {
-    var summary = $(this).data('mokuji-summary');
+    $mokuji = $(this);
 
-    $(this).append('<ul class="pl-3"><h3 class="normal">目次</h3></ul>');
+    $mokuji.html('');
+    $mokuji.append('<ul class="pl-3"><h3 class="normal">目次</h3></ul>');
 
-    if(summary == true) {
-      $(this).after('<h2 class="normal" summary="true">まとめ(直近10個)</h2>');
-    }
+    buildSummary($mokuji, $(this).data('mokuji-summary'));
 
     var items = [];
     $('h2.normal').each(function(idx){
       $(this).attr("id", "mokuji"+idx);
-      items.push('<a href="#mokuji' + idx + '">' + $(this).text() + '</a>');
+      items.push('<a href="#mokuji' + idx + '" data-turbolinks="false">' + $(this).text() + '</a>');
     });
-    $("ul", this).append('<li>' + items.join("</li><li>") + '</li>');
-
-    // すべてのカテゴリから評価された日付降順に抽出する「まとめ」
-    if(summary == true) {
-      var data = [];
-      $(".nice_content_wrap").each(function(){
-        data.push({"id" : $(this).attr("id"), "time" : new Date($(this).attr("time"))});
-      });
-      data = data.sort(function(x, y){return x.time < y.time ? 1 : -1});
-      for(var i = 9; i >= 0; i--){
-        $obj = $("#" + data[i].id).clone();
-        $obj.attr("id","");
-        $('h2.normal[summary=true]').after($obj);
-      }
-    }
+    $("ul", $mokuji).append('<li>' + items.join("</li><li>") + '</li>');
   }
 }(jQuery))
 
+// すべてのカテゴリから評価された日付降順に抽出する「まとめ」
+buildSummary = function($mokuji, summary) {
+  if(summary == false) {
+    return;
+  }
+
+  $('#summary-wrapper').remove();
+  $mokuji.after('<div id="summary-wrapper">');
+
+  var title = 'まとめ(直近10個)';
+  $('#summary-wrapper').append('<h2 id="summary-title" class="normal">' + title + '</h2>');
+  $('#summary-wrapper').append('<div id="summary-contents" class="well well-small">');
+
+  var data = [];
+  $(".nice_content_wrap").each(function(){
+    data.push({"id" : $(this).attr("id"), "time" : new Date($(this).attr("time"))});
+  });
+  data = data.sort(function(x, y){return x.time < y.time ? 1 : -1});
+  for(var i = 9; i >= 0; i--){
+    $obj = $("#" + data[i].id).clone();
+    $obj.attr("id","");
+    $('#summary-contents').append($obj);
+  }
+}
