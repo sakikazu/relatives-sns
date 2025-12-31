90d002fd8d1eb76f2153f0ce5b74753eca9bb2d3
k.sakimura <sakikazu15@gmail.com>
2025-12-31 10:33:09 +0900

    ActionCableで画像URLのホストが間違ってたので修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # ActiveStorage URL generation for ActionCable-rendered views.
+  Rails.application.routes.default_url_options[:host] = "localhost"
+  Rails.application.routes.default_url_options[:port] = 3000
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
